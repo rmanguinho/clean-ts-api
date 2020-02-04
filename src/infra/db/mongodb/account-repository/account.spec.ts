93bb1c37140280fc0e083b2ee7354481575dd2c8
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-02-04 15:49:46 -0300

    test: ensure AccountMongoRepository returns null if loadByEmail returns null

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -50,4 +50,10 @@ describe('Account Mongo Repository', () => {
     expect(account.email).toBe('any_email@mail.com')
     expect(account.password).toBe('any_password')
   })
+
+  test('Should return null if loadByEmail fails', async () => {
+    const sut = makeSut()
+    const account = await sut.loadByEmail('any_email@mail.com')
+    expect(account).toBeFalsy()
+  })
 })
