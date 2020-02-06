ac390f8f2c624e2366c35dc42c003429826037f3
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-02-06 13:52:38 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'rodrigo.manguinho@gmail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
