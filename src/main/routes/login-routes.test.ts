c6a04cd6625e761d747686274da0827036f94428
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-04-06 15:51:44 -0300

    test: ensure /signup route returns 403 if email is already in use

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -31,6 +31,15 @@ describe('Login Routes', () => {
           passwordConfirmation: '123'
         })
         .expect(200)
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Rodrigo',
+          email: 'rodrigo.manguinho@gmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(403)
     })
   })
 
