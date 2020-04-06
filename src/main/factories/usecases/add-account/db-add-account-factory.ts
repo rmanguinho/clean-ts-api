547a1bde6bf9ffe079d913dab1d5e1a53d679ea7
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-04-06 15:49:24 -0300

    fix: add AccountMongoRepository on DbAddAccount composition

--- src/main/factories/usecases/add-account/db-add-account-factory.ts
+++ src/main/factories/usecases/add-account/db-add-account-factory.ts
@@ -7,5 +7,5 @@ export const makeDbAddAccount = (): AddAccount => {
   const salt = 12
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
-  return new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  return new DbAddAccount(bcryptAdapter, accountMongoRepository, accountMongoRepository)
 }
