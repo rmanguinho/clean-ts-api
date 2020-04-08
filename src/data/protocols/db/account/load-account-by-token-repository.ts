3a8bcd810d5f768a20aff89c94167f409ca0f463
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-04-08 17:55:17 -0300

    feat: ensure DbLoadAccountByToken calls LoadAccountByTokenRepository with correct values

---
+++ src/data/protocols/db/account/load-account-by-token-repository.ts
@@ -0,0 +1,5 @@
+import { AccountModel } from '../../../../domain/models/account'
+
+export interface LoadAccountByTokenRepository {
+  loadByToken (token: string, role?: string): Promise<AccountModel>
+}
--- src/data/usecases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -1,12 +1,19 @@
 import { LoadAccountByToken } from '../../../domain/usecases/load-account-by-token'
 import { AccountModel } from '../../../domain/models/account'
 import { Decrypter } from '../../protocols/criptography/decrypter'
+import { LoadAccountByTokenRepository } from '../../protocols/db/account/load-account-by-token-repository'
 
 export class DbLoadAccountByToken implements LoadAccountByToken {
-  constructor (private readonly decrypter: Decrypter) {}
+  constructor (
+    private readonly decrypter: Decrypter,
+    private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository
+  ) {}
 
   async load (accessToken: string, role?: string): Promise<AccountModel> {
-    await this.decrypter.decrypt(accessToken)
+    const token = await this.decrypter.decrypt(accessToken)
+    if (token) {
+      await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
+    }
     return null
   }
 }
