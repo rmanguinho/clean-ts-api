a8de8e50a7d0d94b17c38bf06e0fc6cba3b27bf2
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-04-10 11:03:25 -0300

    test: ensure JwtAdapter returns a decrypted value on verify success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -47,5 +47,11 @@ describe('Jwt Adapter', () => {
       await sut.decrypt('any_token')
       expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
     })
+
+    test('Should return a value on verify success', async () => {
+      const sut = makeSut()
+      const value = await sut.decrypt('any_token')
+      expect(value).toBe('any_value')
+    })
   })
 })
