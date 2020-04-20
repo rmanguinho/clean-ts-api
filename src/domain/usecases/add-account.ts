4cf6e585c209fcff3e32105c05d2cb539ec69c9f
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-04-20 09:50:03 -0300

    refactor: change models to use Omit

--- src/domain/usecases/add-account.ts
+++ src/domain/usecases/add-account.ts
@@ -1,10 +1,6 @@
 import { AccountModel } from '@/domain/models/account'
 
-export type AddAccountModel = {
-  name: string
-  email: string
-  password: string
-}
+export type AddAccountModel = Omit<AccountModel, 'id'>
 
 export interface AddAccount {
   add (account: AddAccountModel): Promise<AccountModel>
--- src/domain/usecases/add-survey.ts
+++ src/domain/usecases/add-survey.ts
@@ -1,10 +1,6 @@
-import { SurveyAnswerModel } from '@/domain/models/survey'
+import { SurveyModel } from '@/domain/models/survey'
 
-export type AddSurveyModel = {
-  question: string
-  answers: SurveyAnswerModel[]
-  date: Date
-}
+export type AddSurveyModel = Omit<SurveyModel, 'id'>
 
 export interface AddSurvey {
   add (data: AddSurveyModel): Promise<void>
