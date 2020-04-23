ca86d8bea1c1a1a1729d885adf7734337fa612bf
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-04-23 10:44:07 -0300

    refactor: remove duplicated folder

--- src/data/usecases/save-survey-result/db-save-survey-result-protocols.ts
+++
@@ -1,3 +0,0 @@
-export * from '@/domain/usecases/survey-result/save-survey-result'
-export * from '@/domain/models/survey-result'
-export * from '@/data/protocols/db/survey-result/save-survey-result-repository'
