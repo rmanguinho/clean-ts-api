ccdf059d9c8a507cb754ca6f3c59434a85e3c1da
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-05-01 13:28:07 -0300

    feat: ensure LoadSurveyResultController calls LoadSurveyById with correct id

---
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller-protocols.ts
@@ -0,0 +1,2 @@
+export * from '@/presentation/protocols'
+export * from '@/domain/usecases/survey/load-survey-by-id'
---
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -0,0 +1,10 @@
+import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './load-survey-result-controller-protocols'
+
+export class LoadSurveyResultController implements Controller {
+  constructor (private readonly loadSurveyById: LoadSurveyById) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+    return Promise.resolve(null)
+  }
+}
