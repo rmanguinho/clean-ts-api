6f2d766d18c6d643485fb2bd3c2d23e226c7ba92
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-05-25 10:51:02 -0300

    docs: update login and signup schema

--- src/main/docs/schemas/account-schema.ts
+++ src/main/docs/schemas/account-schema.ts
@@ -3,7 +3,10 @@ export const accountSchema = {
   properties: {
     accessToken: {
       type: 'string'
+    },
+    name: {
+      type: 'string'
     }
   },
-  required: ['accessToken']
+  required: ['accessToken', 'name']
 }
