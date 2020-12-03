62e84f8694c0630318e6c7a8031ffedd21d95998
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-12-03 16:03:40 -0300

    refactor: extract code to helper method

--- src/data/usecases/db-load-survey-result.ts
+++ src/data/usecases/db-load-survey-result.ts
@@ -1,5 +1,5 @@
 import { LoadSurveyResult } from '@/domain/usecases'
-import { SurveyResultModel } from '@/domain/models'
+import { SurveyModel, SurveyResultModel } from '@/domain/models'
 import { LoadSurveyResultRepository, LoadSurveyByIdRepository } from '@/data/protocols'
 
 export class DbLoadSurveyResult implements LoadSurveyResult {
@@ -12,18 +12,22 @@ export class DbLoadSurveyResult implements LoadSurveyResult {
     let surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(surveyId, accountId)
     if (!surveyResult) {
       const survey = await this.loadSurveyByIdRepository.loadById(surveyId)
-      surveyResult = {
-        surveyId: survey.id,
-        question: survey.question,
-        date: survey.date,
-        answers: survey.answers.map(answer => ({
-          ...answer,
-          count: 0,
-          percent: 0,
-          isCurrentAccountAnswer: false
-        }))
-      }
+      surveyResult = this.makeEmptyResult(survey)
     }
     return surveyResult
+  }
+
+  private makeEmptyResult (survey: SurveyModel): SurveyResultModel {
+    return {
+      surveyId: survey.id,
+      question: survey.question,
+      date: survey.date,
+      answers: survey.answers.map(answer => ({
+        ...answer,
+        count: 0,
+        percent: 0,
+        isCurrentAccountAnswer: false
+      }))
+    }
   }
 }
