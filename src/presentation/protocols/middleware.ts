0ed52f1f43a62b0c4d1b9535951e54283cb5e21d
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-12-07 14:37:47 -0300

    fix: change Middleware to use generics

--- src/presentation/protocols/middleware.ts
+++ src/presentation/protocols/middleware.ts
@@ -1,5 +1,5 @@
-import { HttpRequest, HttpResponse } from '@/presentation/protocols'
+import { HttpResponse } from '@/presentation/protocols'
 
-export interface Middleware {
-  handle: (httpRequest: HttpRequest) => Promise<HttpResponse>
+export interface Middleware<T = any> {
+  handle: (httpRequest: T) => Promise<HttpResponse>
 }
