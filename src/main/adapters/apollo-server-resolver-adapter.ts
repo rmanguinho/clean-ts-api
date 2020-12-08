436a71aafbf825546cc667becfa197d2107f08a3
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-12-08 11:44:07 -0300

    feat: add status code error on exceptions

--- src/main/adapters/apollo-server-resolver-adapter.ts
+++ src/main/adapters/apollo-server-resolver-adapter.ts
@@ -1,6 +1,15 @@
 import { Controller } from '@/presentation/protocols'
 
+import { UserInputError, AuthenticationError, ForbiddenError, ApolloError } from 'apollo-server-express'
+
 export const adaptResolver = async (controller: Controller, args: any): Promise<any> => {
   const httpResponse = await controller.handle(args)
-  return httpResponse.body
+  switch (httpResponse.statusCode) {
+    case 200:
+    case 204: return httpResponse.body
+    case 400: throw new UserInputError(httpResponse.body.message)
+    case 401: throw new AuthenticationError(httpResponse.body.message)
+    case 403: throw new ForbiddenError(httpResponse.body.message)
+    default: throw new ApolloError(httpResponse.body.message)
+  }
 }
--- src/main/config/apollo-server.ts
+++ src/main/config/apollo-server.ts
@@ -3,11 +3,36 @@ import resolvers from '@/main/graphql/resolvers'
 
 import { ApolloServer } from 'apollo-server-express'
 import { Express } from 'express'
+import { GraphQLError } from 'graphql'
+
+const handleErrors = (response: any, errors: readonly GraphQLError[]): void => {
+  errors?.forEach(error => {
+    response.data = undefined
+    if (checkError(error, 'UserInputError')) {
+      response.http.status = 400
+    } else if (checkError(error, 'AuthenticationError')) {
+      response.http.status = 401
+    } else if (checkError(error, 'ForbiddenError')) {
+      response.http.status = 403
+    } else {
+      response.http.status = 500
+    }
+  })
+}
+
+const checkError = (error: GraphQLError, errorName: string): boolean => {
+  return [error.name, error.originalError?.name].some(name => name === errorName)
+}
 
 export default (app: Express): void => {
   const server = new ApolloServer({
     resolvers,
-    typeDefs
+    typeDefs,
+    plugins: [{
+      requestDidStart: () => ({
+        willSendResponse: ({ response, errors }) => handleErrors(response, errors)
+      })
+    }]
   })
   server.applyMiddleware({ app })
 }
