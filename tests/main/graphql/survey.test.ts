81b4d53a0ae2b98f2f7fd3f54cc746a337f68d8f
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-12-11 15:02:35 -0300

    test: ensure Surveys Query returns AccessDeniedError if no token is provided

--- tests/main/graphql/survey.test.ts
+++ tests/main/graphql/survey.test.ts
@@ -98,5 +98,23 @@ describe('Survey GraphQL', () => {
         image: null
       }])
     })
+
+    test('Should return AccessDeniedError if no token is provided', async () => {
+      const now = new Date()
+      await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        }, {
+          answer: 'Answer 2'
+        }],
+        date: now
+      })
+      const { query } = createTestClient({ apolloServer })
+      const res: any = await query(surveysQuery)
+      expect(res.data).toBeFalsy()
+      expect(res.errors[0].message).toBe('Access denied')
+    })
   })
 })
