377f89b416a83c2d450ed0e38e517d1bda4f8648
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-12-11 14:29:12 -0300

    test: ensure Login Query returns UnauthorizedError on invalid credentials

--- tests/main/graphql/login.test.ts
+++ tests/main/graphql/login.test.ts
@@ -51,5 +51,17 @@ describe('Login GraphQL', () => {
       expect(res.data.login.accessToken).toBeTruthy()
       expect(res.data.login.name).toBe('Rodrigo')
     })
+
+    test('Should return UnauthorizedError on invalid credentials', async () => {
+      const { query } = createTestClient({ apolloServer })
+      const res: any = await query(loginQuery, {
+        variables: {
+          email: 'rodrigo.manguinho@gmail.com',
+          password: '123'
+        }
+      })
+      expect(res.data).toBeFalsy()
+      expect(res.errors[0].message).toBe('Unauthorized')
+    })
   })
 })
