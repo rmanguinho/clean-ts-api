7d50ad79f7fe6e3bc5fbef63e4e35eab7a17c980
Guilherme Ledes <ledes.guilherme@gmail.com>
2021-12-20 16:02:26 -0300

    test: ensure LoadSurveyResultController to throw any throw

--- tests/presentation/controllers/load-survey-result-controller.spec.ts
+++ tests/presentation/controllers/load-survey-result-controller.spec.ts
@@ -1,11 +1,10 @@
 import { LoadSurveyResultController } from '@/presentation/controllers'
-import { forbidden, serverError, ok } from '@/presentation/helpers'
 import { InvalidParamError } from '@/presentation/errors'
-import { CheckSurveyByIdSpy, LoadSurveyResultSpy } from '@/tests/presentation/mocks'
+import { forbidden, ok } from '@/presentation/helpers'
 import { throwError } from '@/tests/domain/mocks'
-
-import MockDate from 'mockdate'
+import { CheckSurveyByIdSpy, LoadSurveyResultSpy } from '@/tests/presentation/mocks'
 import faker from 'faker'
+import MockDate from 'mockdate'
 
 const mockRequest = (): LoadSurveyResultController.Request => ({
   accountId: faker.datatype.uuid(),
@@ -52,11 +51,11 @@ describe('LoadSurveyResult Controller', () => {
     expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')))
   })
 
-  test('Should return 500 if CheckSurveyById throws', async () => {
+  test('Should throw if CheckSurveyById throws', async () => {
     const { sut, checkSurveyByIdSpy } = makeSut()
     jest.spyOn(checkSurveyByIdSpy, 'checkById').mockImplementationOnce(throwError)
-    const httpResponse = await sut.handle(mockRequest())
-    expect(httpResponse).toEqual(serverError(new Error()))
+    const promise = sut.handle(mockRequest())
+    await expect(promise).rejects.toThrow()
   })
 
   test('Should call LoadSurveyResult with correct values', async () => {
@@ -67,11 +66,11 @@ describe('LoadSurveyResult Controller', () => {
     expect(loadSurveyResultSpy.accountId).toBe(request.accountId)
   })
 
-  test('Should return 500 if LoadSurveyResult throws', async () => {
+  test('Should throw if LoadSurveyResult throws', async () => {
     const { sut, loadSurveyResultSpy } = makeSut()
     jest.spyOn(loadSurveyResultSpy, 'load').mockImplementationOnce(throwError)
-    const httpResponse = await sut.handle(mockRequest())
-    expect(httpResponse).toEqual(serverError(new Error()))
+    const promise = sut.handle(mockRequest())
+    await expect(promise).rejects.toThrow()
   })
 
   test('Should return 200 on success', async () => {
