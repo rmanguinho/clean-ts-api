c41684386482d979dead6a0115052aab7878ec8b
Guilherme Ledes <ledes.guilherme@gmail.com>
2021-12-20 16:14:40 -0300

    refactor: replace LogControllerDecorator by ServerErrorControllerDecorator

--- src/main/decorators/index.ts
+++ src/main/decorators/index.ts
@@ -1,2 +1,1 @@
-export * from './log-controller-decorator'
 export * from './server-error-controller-decorator'
--- src/main/factories/controllers/signup-controller-factory.ts
+++ src/main/factories/controllers/signup-controller-factory.ts
@@ -1,8 +1,8 @@
-import { makeDbAuthentication, makeSignUpValidation, makeLogControllerDecorator, makeDbAddAccount } from '@/main/factories'
+import { makeDbAuthentication, makeSignUpValidation, makeServerErrorControllerDecorator, makeDbAddAccount } from '@/main/factories'
 import { SignUpController } from '@/presentation/controllers'
 import { Controller } from '@/presentation/protocols'
 
 export const makeSignUpController = (): Controller => {
   const controller = new SignUpController(makeDbAddAccount(), makeSignUpValidation(), makeDbAuthentication())
-  return makeLogControllerDecorator(controller)
+  return makeServerErrorControllerDecorator(controller)
 }
